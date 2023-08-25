40631ceab15f4550328fd22c65709870f1c1d081
Hexona <xuanjiap@lolicon.ac.cn>
2023-08-26 02:48:55 +0800

    feat: shorthand command

--- src/menu/apex/event.ts
+++ src/menu/apex/event.ts
@@ -1,3 +1,30 @@
 import { client } from "init/client";
+import apexConnect from "./command/connect";
+import apexSearch from "./command/search";
 
-client.on('message.text', (event) => { });
+client.on('message.text', (event) => {
+    switch (true) {
+        case /^绑定(origin|pc|xbl|xbox|psn|ps)? ?(.+)$/.test(event.content): {
+            const [platform, username] = getMatches(event.content, /^绑定(origin|pc|xbl|xbox|psn|ps)? ?(.+)$/);
+            apexConnect.exec([username, platform], event, client);
+            break;
+        };
+        case /^查[询找](origin|pc|xbl|xbox|psn|ps)? ?(.+)?$/.test(event.content): {
+            const [platform, username] = getMatches(event.content, /^查[询找](origin|pc|xbl|xbox|psn|ps)? ?(.+)?$/);
+            apexSearch.exec([username, platform], event, client);
+            break;
+        }
+    }
+});
+
+
+function getMatches(string: string, regex: RegExp): string[] {
+    let matches = regex.exec(string);
+    let res = [];
+    if (matches) {
+        for (let i = 1; i < matches.length; ++i) {
+            res.push(matches[i]);
+        }
+    }
+    return res;
+}
